e89aab3687292024db76f67e8bf679d8501e0aa5
AMARTA KUNDU <73887575+AMARTA7@users.noreply.github.com>
2022-01-31 21:52:26 +0530

    Day-01 Assignment

---
+++ Practice/Assignment/Day-1.py
@@ -0,0 +1,4 @@
+# AMARTA KUNDU
+# Question for day-01: Take multiple values as input and print sum of them.
+
+print('The result is: {}'.format(sum(map(int, input('Enter the numbers: ').split(',')))))
